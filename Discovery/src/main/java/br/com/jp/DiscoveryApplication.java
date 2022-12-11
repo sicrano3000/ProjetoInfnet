35702ad0b276992cb2ece6ce675926f9d4765165
Jonathan Philipe <sicrano3000@gmail.com>
2022-12-11 13:52:04 -0300

    Configurando Eureka

--- Discovery/src/main/java/br/com/jp/DiscoveryApplication.java
+++ Discovery/src/main/java/br/com/jp/DiscoveryApplication.java
@@ -2,14 +2,14 @@ package br.com.jp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ProdutosApplication {
+@EnableEurekaServer
+public class DiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProdutosApplication.class, args);
+		SpringApplication.run(DiscoveryApplication.class, args);
 	}
 
 }
